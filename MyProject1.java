48fee3114e1ed190ee56ed261e843fc5f27833ea
Taned <gamegamethanet@gmail.com>
2023-03-22 12:05:39 +0700

    Taned027_Revert

--- MyProject1.java
+++ MyProject1.java
@@ -14,10 +14,8 @@ public class MyProject1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Worlds!!!");
-        System.out.println("Hello Java!!!");
-        System.out.println("Hello Programming!!!");
-        System.out.println("Hello Python!!!");
+        System.out.println("Hello Worlds!");
+        System.out.println("Hello Java");
     }
     
 }
